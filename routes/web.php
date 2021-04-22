8e40a587d52bb54962af4ed4b529c4f1575faa48
ayaka.fujimoto <fujimoto@coresite.ne.jp>
2021-04-22 09:13:53 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
+
+
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
